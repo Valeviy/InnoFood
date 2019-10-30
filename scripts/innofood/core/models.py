1136e91d692741b3aec2909457cf8b5f822e0599
Valeria <lera0349314@yandex.ru>
2019-10-30 19:37:50 +0300

    Added frontend part for manager

--- scripts/innofood/core/models.py
+++ scripts/innofood/core/models.py
@@ -12,17 +12,17 @@ from django.conf import settings
 Admin class goes to the admin.py because of the Django's architecture
 """
 
-class InnoFoodUser(AbstractBaseUser):
-    username = models.CharField(max_length=100, unique=True)
-    visible = models.BooleanField(default=True)
-    USERNAME_FIELD = 'username'
+# class InnoFoodUser(AbstractBaseUser):
+#     username = models.CharField(max_length=100, unique=True)
+#     visible = models.BooleanField(default=True)
+#     USERNAME_FIELD = 'username'
 
-class Customer(InnoFoodUser):
-    address = models.CharField(max_length=400)
-    phone_number = models.CharField(max_length=12)
+# class Customer(InnoFoodUser):
+#     address = models.CharField(max_length=400)
+#     phone_number = models.CharField(max_length=12)
 
-    def create_order(self):
-        pass
+#     def create_order(self):
+#         pass
 
 
 
@@ -31,18 +31,21 @@ class Customer(InnoFoodUser):
 class Cafe(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=400)
-    # manager = models.OneToOneField(Manager, on_delete=models.CASCADE)
+    manager = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True, blank=True)
     visible = models.BooleanField(default=True)
 
     def delete_cafe(self):
         pass
 
+    def __str__(self):
+        return self.name
 
-class Manager(models.Model):
-    # Cafe is in brackets because it's the case when two models reference each other (Manager and Cafe)
-    assigned_cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    visible = models.BooleanField(default=True)
+
+# class Manager(models.Model):
+#     # Cafe is in brackets because it's the case when two models reference each other (Manager and Cafe)
+#     assigned_cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     visible = models.BooleanField(default=True)
 
 
 
@@ -58,27 +61,26 @@ class Dish(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     visible = models.BooleanField(default=True)
+    in_menu = models.BooleanField(default=True)
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
-    # manager = models.ForeignKey(Ma)
+    # manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
     def edit_dish(self):
         pass
-
-
-class OrderDetail(models.Model):
-    dishes = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
-    def get_all_dishes(self):
-        pass
-
 
 class Order(models.Model):
     destination = models.CharField(max_length=400)
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     confirmed = models.BooleanField(default=False)
-    parameter = models.OneToOneField(OrderDetail, on_delete=models.CASCADE)
+    # parameter = models.OneToOneField(OrderDetail, on_delete=models.CASCADE)
     visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.destination) + ' ' + str(self.customer)
 
     def delete_order(self):
         pass
@@ -92,6 +94,14 @@ class Order(models.Model):
     def confirm_order(self):
         pass
 
+
+class OrderDetail(models.Model):
+    dishes = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def get_all_dishes(self):
+        pass
 
 class Complaint(models.Model):
     description = models.TextField(max_length=500)
--- scripts/innofood/innofood/urls.py
+++ scripts/innofood/innofood/urls.py
@@ -24,25 +24,29 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.registration_view, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    # path('redirectlog/', views.redirectlog, name='redirectlog'),
     # CUSTOMER PART
     path('cafes/', views.CafeListView.as_view(), name='cafes'),
     path('cafes/<int:id>/', views.DishListView.as_view(), name='dishes'),
     # path('orders/', ..., name='customer_orders'),
     # path('complaint/', ..., name='customer_new_complaint'),
     path('cart/', views.CartListView.as_view(), name='cart'),
-    path('new_order/', views.create_order, name='new_order'),
+
+#    path('new_order/', views.create_order, name='new_order'),
+    path('cafes/<int:id>/new_order/', views.create_order, name='new_order'),
     path('account/', views.user_account_change, name='customer_account'),
     # path('complaints/', ..., name='customer_complaints'),
 
     # MANAGER PART
-    path('manager/orders', views.ManagerOrders.as_view(), name='manager_orders'),
-    path('manager/orders/<int:confirmed>', views.ManagerOrdersStatus.as_view(), name='manager_orders_status'),
-    # path('manager/complaints/<status:bool>', ..., name='manager_complaints')
-
-    # ADMIN PART
-    # path('admin/cafes/', ..., name='admin_cafes')
-    # path('admin/managers/', ..., name='admin_managers')
+    path('manager/orders/', views.ManagerOrders.as_view(), name='manager_orders'),
+    path('manager/confirmed/', views.ManagerOrdersConfirmed.as_view(), name='manager_confirmed'),
+    path('manager/declined/', views.ManagerOrdersDeclined.as_view(), name='manager_declined'),
+    path('manager/switch_order/<int:id>/<int:status>', views.switch_order, name='switch_order'),
+    path('manager/cafe/', views.ManagerCafe.as_view(), name='manager_cafe'),
+    path('manager/dish/', views.ManagerDish.as_view(), name='manager_dish'),
+    path('manager/update_dish/<int:pk>/', views.ManagerDishUpdate.as_view(), name='manager_dish_update'),
+    path('manager/showhide_dish/<int:id>/', views.showhide_dish, name='showhide_dish'),
+    path('manager/delete_dish/<int:id>/', views.delete_dish, name='delete_dish'),
 
     path('admin/', admin.site.urls),
 
